eae65650aead1ab35cdacce364fd0dc9d90154d0
Gustav Fléron <flerongustav@gmail.com>
2024-11-25 11:35:56 +0100

    Yolo + Kalman running
    Almost good to go. Currently only tracking pedestrians, but can easily be configured to include bikes and cars (change to class_indexes = [0, 1, 2])
    Having trouble with depth measurements, causing the kalman filter to mess up.
    May need further tuning of filter, could also benefit from implementation of "deletion zone" around border of image.

--- Kalman.py
+++ Kalman.py
@@ -30,15 +30,15 @@ class KalmanFilter:
         # x = [x, y, z, dx, dy, dz, ddx, ddy, ddz]
         # Initially everything is 0.
         self.X = np.zeros((9, 1))
-        print("Shape of x: ", np.shape(self.X), "(Should be 9x1)")
+        #print("Shape of x: ", np.shape(self.X), "(Should be 9x1)")
 
         # The initial uncertainty (9x9).
-        self.P = np.identity(9) * 1  # The "*1" is a factor. Tweak if needed.
-        print("Shape of P: ", np.shape(self.P), "(Should be 9x9)")
+        self.P = np.identity(9) * 0.1  # The "*1" is a factor. Tweak if needed.
+        #print("Shape of P: ", np.shape(self.P), "(Should be 9x9)")
 
         # The external motion (9x1).
-        self.u = np.transpose(np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0]]))
-        print("Shape of u: ", np.shape(self.u), "(Should be 9x1)")
+        self.u = np.transpose(np.array([[0.1, 0.1, 0.1, 0, 0, 0, 0, 0, 0]]))
+        #print("Shape of u: ", np.shape(self.u), "(Should be 9x1)")
 
         # The transition matrix (9x9).
         self.F = np.array([[1, 0, 0, self.dt, 0, 0, 0.5 * pow(self.dt, 2), 0, 0],
@@ -50,32 +50,40 @@ class KalmanFilter:
                       [0, 0, 0, 0, 0, 0, 1, 0, 0],
                       [0, 0, 0, 0, 0, 0, 0, 1, 0],
                       [0, 0, 0, 0, 0, 0, 0, 0, 1]])
-        print("Shape of F: ", np.shape(self.F), "(Should be 9x9)")
+        #print("Shape of F: ", np.shape(self.F), "(Should be 9x9)")
 
         # The observation matrix (3x9).
         self.H = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0, 0]])
-        print("Shape of H: ", np.shape(self.H), "(Should be 3x9)")
+        #print("Shape of H: ", np.shape(self.H), "(Should be 3x9)")
 
         # The measurement uncertainty.
-        self.R = 3.5  # I dunno
+        self.R = 3  # I dunno
 
     def update(self):
         self.error = self.Z - self.H @ self.X
+        #print("self.error", self.error)
         Ht = np.transpose(self.H)
         self.S = self.H @ self.P @ Ht + self.R
+        #print("self.S", self.S)
 
         if np.linalg.det(self.S) != 0:
             self.K = self.P @ Ht @ np.linalg.inv(self.S)
         else:
-            self.K = self.P @ Ht @ np.array([[0.000001, 0.000001], [0.000001, 0.000001]])
-        I = np.identity(6)  # 6 because our system is 6-dimensional
+            eps = 1e-10 * np.eye(self.S.shape[0])
+            self.K = self.P @ Ht @ np.linalg.inv(self.S + eps)
+        I = np.identity(9)  # 9 because our system is 9-dimensional
 
-        self.Xpred = self.X + self.K @ self.error
-        self.Ppred = (I - self.K @ self.H) @ self.P
+        #print("self.K", self.K)
+        #print("udregningen:", self.K @ self.error)
+
+        self.X = self.X + self.K @ self.error
+        self.P = (I - self.K @ self.H) @ self.P
 
     def predict(self):
-        self.Xpred = self.F @ self.X + self.u
-        self.Ppred = self.F @ self.P @ np.transpose(self.F)
+        #print("F", self.F)
+        #print("u", self.u)
+        self.X = self.F @ self.X + self.u
+        self.P = self.F @ self.P @ np.transpose(self.F)
 
 
 
--- main.py
+++ main.py
@@ -8,13 +8,13 @@ import Kalman
 
 
 def within_margin(pred_x, pred_y, act_x, act_y):
-    margin = 50
+    margin = 20
     if pred_x + margin >= act_x or pred_x - margin <= act_x and pred_y + margin >= act_y or pred_y - margin <= act_y:
         return True
     else:
         return False
 
-def center(x1, y1, x2, y2):
+def get_center(x1, y1, x2, y2):
     center_x = (x1+x2) / 2
     center_y = (y1+y2) / 2
 
@@ -30,7 +30,7 @@ def get_depth_from_bbox(depth_calculator, results):
         box = result.boxes.xyxy[0]
         x1, y1, x2, y2 = np.array(box).astype(int)
 
-        center_coords = center(x1, y1, x2, y2)
+        center_coords = get_center(x1, y1, x2, y2)
         centers.append(center_coords)
 
         cls_idx = result.boxes.cls.item()
@@ -57,7 +57,8 @@ if __name__ == "__main__":
     data = load_data.StereoVisionData()
 
     model = YOLO("yolo11n.pt")
-    class_indexes = [0, 1, 2]   #Person, Bicycle, Car indexes in YOLO model :)
+    #class_indexes = [0, 1, 2]   #Person, Bicycle, Car indexes in YOLO model :)
+    class_indexes = [0]
 
     KF_list = []
     previous_class_names = []
@@ -74,20 +75,25 @@ if __name__ == "__main__":
         DepthObj.run_without_fuckery(frame_data["left_image"], frame_data["right_image"], calibration_data_path)
 
         class_names, confidences, weighted_depths, centers = get_depth_from_bbox(DepthObj, results)
-
+        #print("KF_LIST1:", KF_list)
         if class_names is not None:
             new_KF_list = [None]*len(class_names)
             for j in range(len(class_names)):
+
                 min_dist = 99999
                 KF_match_index = -1
                 KF_taken = []
+                center = centers[j]
+                #print("KF_LIST2:", KF_list)
                 for q in range(len(KF_list)):
+                    #print("KF_LIST3:", KF_list)
+                    if not KF_list:
+                        continue
                     if KF_list[q].class_name != class_names[j]:
                         continue
                     if q in KF_taken:
                         continue
-                    predicted_pos = KF_list[q].X_pred()
-                    center = centers[j]
+                    predicted_pos = [KF_list[q].X[0], KF_list[q].X[1], KF_list[q].X[2]]
 
                     if not within_margin(predicted_pos[0], predicted_pos[1], center[0], center[1]):
                         continue
@@ -103,20 +109,43 @@ if __name__ == "__main__":
                 else:
                     new_KF_list[j] = Kalman.KalmanFilter()
                     new_KF_list[j].class_name = class_names[j]
+                    new_KF_list[j].X = np.array([[center[0]], [center[1]], [weighted_depths[j]], [0], [0], [0], [0], [0], [0]])
+                    print("new_KF_list[j].X", new_KF_list[j].X)
 
-                KF_list = new_KF_list.copy()
+            KF_list = new_KF_list.copy()
 
-        for w in range(len(new_KF_list)):
-            print("KF LIST CLASS NAMES:", w, new_KF_list[w].class_name)
+            for j in range(len(KF_list)):
+                center = centers[j]
+                # draw detection
+                cv2.circle(frame_data["left_image"], (int(center[0]),int(center[1])), 10, (0, 255, 0), 3)
+
+                #print("Center:", center)
+                KF_list[j].Z = np.array([[center[0]], [center[1]], [weighted_depths[j]]])
+                #print("Shape of Z: ", np.shape(KF_list[j].Z), KF_list[j].Z)
+
+                KF_list[j].update()
+                KF_list[j].predict()
+
+                #print("Output from KF: ",KF_list[j].X)
+
+                circle_center_coords = (int(KF_list[j].X[0]), int(KF_list[j].X[1]))
+                #print("circle center", circle_center_coords)
+                print("Predicted distances", KF_list[j].X[2])
+                print("Measured distances", weighted_depths[j])
+                cv2.circle(frame_data["left_image"], circle_center_coords, max(int(300/KF_list[j].X[2]),5), (0,0,255), 3)
+
+                #x = KF_list.Xpred
+                #P = KF_list.Ppred
+            cv2.imshow("frame",frame_data["left_image"])
+            cv2.waitKey(0)
 
 
-
-
-
+        #for w in range(len(new_KF_list)):
+            #print("KF LIST CLASS NAMES:", w, new_KF_list[w].class_name)
 
         previous_class_names = class_names
 
-        print("CLASS NAMES: ", class_names)
+        #print("CLASS NAMES: ", class_names)
 
 
 
